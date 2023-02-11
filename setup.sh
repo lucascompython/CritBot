cd836b4a00a2eb364f5ded89710e5deed8e962de
Lucas de Linhares <lucasdelinhares@gmail.com>
2023-02-11 13:19:16 +0000

    Fix setup.sh problem with logs

--- setup.sh
+++ setup.sh
@@ -10,8 +10,9 @@ wget "https://github.com/freyacodes/Lavalink/releases/download/3.6.2/Lavalink.ja
 echo "Done!"
 
 echo "Installing config files..."
-mv ./config/example_appsettings.yaml ./config/appsettings.yaml
-mv ./config/example_application.yml ./config/application.yml
+cp ./config/example_appsettings.yaml ./config/appsettings.yaml
+cp ./config/example_application.yml ./config/application.yml
+mkdir logs
 echo "{}" > prefixes.json
 echo "{}" > ./i18n/langs.json
 echo "Done!"
