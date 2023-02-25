fd9228b3494766c25a70df44617acbb175dc480a
Lucas de Linhares <lucasdelinhares@gmail.com>
2023-02-25 15:16:25 +0000

    Fix when added to new server sync the server

--- cogs/events.py
+++ cogs/events.py
@@ -15,6 +15,8 @@ class Events(commands.Cog):
         await self.bot.update_prefixes(guild.id, self.bot.default_prefix)
         await self.bot.i18n.update_langs(guild.id, self.bot.default_language)
         self.log(20, f"Joined {guild.name} ({guild.id})")
+        self.tree.copy_global_to(guild=guild)
+        await self.tree.sync(guild=guild)
     
     @commands.Cog.listener()
     async def on_guild_remove(self, guild):
