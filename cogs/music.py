01fa90e5473027639b6fe6ca3c3d34ba9c60a95b
Lucas de Linhares <lucasdelinhares@gmail.com>
2023-08-09 16:06:36 +0100

    Split the play command for platform support

--- cogs/music.py
+++ cogs/music.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
 import discord
+from discord import app_commands
 import wavelink
 from discord.ext import commands
 import asyncio
@@ -8,6 +9,24 @@ import asyncio
 # import the bot class from bot.py
 from bot import CritBot
 from Utils import GeniusLyrics, Paginator, SongNotFound
+from enum import Enum
+
+
+class Platform(Enum):
+    YOUTUBE = ("youtube", "yt")
+    SPOTIFY = ("spotify", "sp")
+    SOUNDCLOUD = ("soundcloud", "sc")
+    YOUTUBE_MUSIC = ("youtube_music", "ytm")
+
+    @classmethod
+    def from_string(cls, value: str) -> "Platform":
+        for platform in cls:
+            if value.lower() in platform.value:
+                return platform
+        return cls.YOUTUBE  # default to Youtube if no match is found
+
+    def __str__(self) -> str:
+        return self.value[0]
 
 
 class Music(commands.Cog):
@@ -16,25 +35,67 @@ class Music(commands.Cog):
         self.t = self.bot.i18n.t
         self.log = self.bot.logger.log
 
+        self.play_cmd = app_commands.Command(
+            name="play",
+            description="command_description",
+            callback=self.play_app_command,
+            extras={
+                "cog_name": "music",
+                "command_name": "play",
+            },
+        )
+        self.bot.tree.add_command(self.play_cmd)
+
         self.genius = GeniusLyrics(self.bot.genius_token, self.bot.web_client)
 
     @commands.Cog.listener()
     async def on_wavelink_node_ready(self, node: wavelink.Node) -> None:
         self.log(20, f"Node <{node.id}> is ready!")
 
-    @commands.hybrid_command(aliases=["p"])
-    async def play(self, ctx: commands.Context, *, query: str) -> None:
+    async def _play_logic(
+        self, ctx: commands.Context, query: str, platform: Platform
+    ) -> None:
         vc: wavelink.Player = await self.join_logic(
             ctx, channel=ctx.author.voice.channel, play=True
         )
-
         query = query.strip("<>")
-        print(query)
 
-        track = await wavelink.Playable.search(query)
-        print(track)
+        print(f"Query: {query}")
+        print(f"Platform: {platform}")
 
-        await vc.play(track[0])
+        # do things...
+
+    async def play_app_command(
+        self,
+        interaction: discord.Interaction,
+        platform: Optional[Platform],
+        *,
+        query: str,
+    ) -> None:
+        await interaction.response.defer()
+        await self._play_logic(
+            await self.bot.get_context(interaction), query, platform or Platform.YOUTUBE
+        )
+
+    @commands.command(aliases=["p"])
+    async def play(
+        self,
+        ctx: commands.Context,
+        *,
+        query: str,
+    ) -> None:
+        arg = query
+        platform = None
+
+        for p in Platform:
+            if (first := arg.split(" ")[0]) in p.value:
+                platform = p
+                query = arg[len(first) + 1 :]
+                break
+
+        platform = platform or Platform.YOUTUBE
+
+        await self._play_logic(ctx, query, platform)
 
     async def join_logic(
         self,
@@ -75,7 +136,7 @@ class Music(commands.Cog):
     @commands.hybrid_command(aliases=["j", "entra"])
     async def join(
         self, ctx: commands.Context, *, channel: Optional[discord.VoiceChannel] = None
-    ) -> None | wavelink.Player:
+    ) -> None:
         await self.join_logic(ctx, channel=channel)
 
     @commands.hybrid_command(aliases=["l", "sai"])
