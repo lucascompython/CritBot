845083c0f837dc21042514501f05a1f9199b43ea
Lucas de Linhares <lucasdelinhares@gmail.com>
2024-10-25 23:36:45 +0100

    Optimize the fetching of the Spotify API by only reading the first 1024 bytes of the response and parsing that

--- Utils/SpotifyTrackInfo.py
+++ Utils/SpotifyTrackInfo.py
@@ -1,6 +1,5 @@
 import lxml.html
 import aiohttp
-import orjson
 
 
 class SpotifyTrackInfo:
@@ -28,11 +27,10 @@ class SpotifyTrackInfo:
         # The token is 115 chars long and ignore the first 16 chars because they are the '{"accessToken":"' part
         access_token = script_content[16:131]
 
-        monthly_listeners = html.xpath('//div[@data-testid="monthly-listeners-label"]')[
-            0
-        ].text_content()[:-18]  # Remove the " monthly listeners" part
-
-        monthly_listeners = monthly_listeners.replace(",", "")
+        # The first 9 chars are "Artist · " and the last 18 are " monthly listeners."
+        monthly_listeners = html.xpath('//meta[@property="og:description"]')[0].attrib[
+            "content"
+        ][9:-19]
 
         return access_token, monthly_listeners
 
@@ -43,36 +41,68 @@ class SpotifyTrackInfo:
             return await resp.text()
 
     async def __get_track_info(
-        self, spotify_track: str, bearer_token: str
-    ) -> tuple[str, str, bool]:
-        """Get the playcount, release date and content rating of a Spotify track.
+        self,
+        spotify_track: str,
+        bearer_token: str,
+    ) -> bytes:
+        """Fetches the track info from the Spotify API. Usually this payload is huge, but we only aproximatapproximately the first 900 bytes. Im still getting the first 1024 just to be sure that songs with long titles are covered.
 
         Args:
             spotify_track (str): The Spotify track ID.
-            bearer_token (str): The bearer token scraped from the website.
+            bearer_token (str): The Spotify access token.
+
+        Returns:
+            bytes: The first 1024 bytes of the response.
+        """
+        url = self.api_partner_url.format(spotify_track=spotify_track)
+        async with self.session.get(
+            url, headers={"authorization": f"Bearer {bearer_token}"}
+        ) as resp:
+            return await resp.content.read(1024)
+
+    def __parse_partial_response(self, data: bytes) -> tuple[str, str, bool]:
+        """Parses the partial response from the Spotify API.
+
+        Args:
+            data (bytes): The first 1024 bytes of the response.
 
         Returns:
             tuple[str, str, bool]: The playcount, release date and content rating. The content rating is a boolean. True if the track is explicit, False if it's not and None if it's unknown.
         """
-        url = self.api_partner_url.format(spotify_track=spotify_track)
+        explicit = data[70] == 69  # 69 is the ascii code for E (EXPLICIT)
 
-        async with self.session.get(
-            url, headers={"authorization": f"Bearer {bearer_token}"}
-        ) as resp:
-            json = await resp.json(loads=orjson.loads)
+        playcount = []
+        date = []
+        current = 159  # 159 is the index of the first byte of the name which is the first "dynamic" field
+        left_brackets = 0
+        done_playcount = False
+        while True:
+            if data[current] == 123:  # Count left brackets
+                left_brackets += 1
 
-        content_rating = None
-        match json["data"]["trackUnion"]["contentRating"]["label"]:
-            case "EXPLICIT":
-                content_rating = True
-            case "NONE":
-                content_rating = False
+            if (
+                left_brackets == 1 and not done_playcount
+            ):  # the playcount is 51 bytes after the first left bracket
+                current += 51
 
-        return (
-            json["data"]["trackUnion"]["playcount"],
-            json["data"]["trackUnion"]["albumOfTrack"]["date"]["isoString"],
-            content_rating,
-        )
+                while data[current] != 34:
+                    playcount.append(chr(data[current]))
+                    current += 1
+                current += 266  # 266 is the max number of "static" bytes between the playcount and the release date this puts the index at the first byte of the copyright text
+                done_playcount = True
+
+            if data[current] == 90:  # 90 is the ascii code for Z
+                current -= 19  # the Z represents the end of the timestamp, so we go back 19 bytes to get the full date
+                for _ in range(20):
+                    date.append(chr(data[current]))
+                    current += 1
+                break
+            current += 1
+
+        playcount = "".join(playcount)
+        release_date = "".join(date)
+
+        return playcount, release_date, explicit
 
     async def get_info(
         self, artist_id: str, spotify_track: str
@@ -91,10 +121,11 @@ class SpotifyTrackInfo:
         bearer_token, monthly_listeners = (
             self.__extract_access_token_and_monthly_listeners(text)
         )
+        response = await self.__get_track_info(spotify_track, bearer_token)
 
         return (
             monthly_listeners,
-            *await self.__get_track_info(spotify_track, bearer_token),
+            *self.__parse_partial_response(response),
         )
 
 
@@ -102,8 +133,8 @@ if __name__ == "__main__":
     import uvloop
 
     async def main() -> None:
-        spotify_track = "7soCc4TpT99bOhJoCiMqFN"
-        artist_id = "2wpWOzQE5TpA0dVnh5YD08"
+        artist_id = "5K4W6rqBFWDnAN6FQUkS6x"
+        spotify_track = "4zfgnW5p7C2QAFauTn09Mh"
 
         async with aiohttp.ClientSession() as session:
             spotify_track_info = SpotifyTrackInfo(session)
