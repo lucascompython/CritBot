464fb77413f43b0101148e525dc7a976f51a9a10
Lucas de Linhares <lucasdelinhares@gmail.com>
2025-10-12 01:41:32 +0100

    rename lang to locale and add index

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,7 +1,9 @@
 CREATE TABLE IF NOT EXISTS guilds(
     id BIGINT PRIMARY KEY,
     prefix CHAR DEFAULT ',',
-    lang VARCHAR(2) DEFAULT 'en',
+    locale VARCHAR(2) DEFAULT 'en',
     sponsorblock_categories TEXT [] DEFAULT '{"sponsor","selfpromo","intro","outro","music_offtopic"}',
     sponsorblock_print_segment_skipped BOOLEAN DEFAULT TRUE
 );
+
+CREATE INDEX IF NOT EXISTS idx_guilds_id ON guilds(id);
